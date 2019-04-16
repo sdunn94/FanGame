7f747d1eef40aca93c917b1016dc452d500eefba
klambert2 <k.lambert98@yahoo.com>
2019-04-16 02:21:34 -0700

    Models, Models, and more Models!
    Updated with a GitHub Plugin for Unity (might not be worth it, but could be helpful too.)  Also added the building model with the start of colliders on the walls and floors, a temp_player object with broken script attached.  It's too late and I'm too brain dead to fix it right now, but figured I'd push what I have so far to my branch.

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float movementSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        movementSpeed = 1f;
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        transform.Translate(movementSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, movementSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
